f5252244713de79df442cf6d983f417250fbe10a
Alexei-Ruzheunikov <alexei_ruzheynikov@mail.ru>
2022-03-25 00:19:39 +0300

    2 Глава Коллекции

--- main.js
+++ main.js
@@ -1,646 +1,257 @@
 "use strict"
 
-// alert("text!");
-// alert("Привет, мир!");
-/*
-alert("Привет, мир!");
-*/
+// let arr  = [];
+
+// let arr = ["пн","вт","ср","чт","пт","сб","вс"];
+
+// let arr = [1,2,3];
+
+// let arr = [1,2,'a','b', null, true, false];
+
+// let arr = ['a', 'b', 'c'];
+// alert(arr);
+// console.log(arr);
+
+// let arr = ["пн", "вт", "ср", "чт", "пт", "сб", "вс"];
+// console.log(arr[0]);
+// console.log(arr[1]);
+// console.log(arr[2]);
+
+// let arr = ["a", "b", "с"];
+// console.log(arr[0]);
+// console.log(arr[1]);
+// console.log(arr[2]);
+
+// let arr = ["a","b","c","d"];
+// console.log(arr[0]+"+"+arr[1]+"+"+arr[2]+"+"+arr[3]);
+
+// let arr = [1,2,3];
+// console.log(arr[0]+arr[1]+arr[2]);
+
+// let arr = [2,5,3,9];
+// let result = arr[0]*arr[1]+arr[2]*arr[3];
+// console.log(result);
+
+// let arr = [1,2,3];
+// console.log(arr.length);
+
+// let arr = [1,2,3,4,5,6];
+// console.log(arr.length);
+
+// let arr = [1,2,3];
+// console.log(arr[arr.length - 1]);
+
+// let arr = [1,2,3,3,2,1,2];
+// console.log(arr[arr.length - 1]);
+
+// let arr = ['a','b','c'];
+// arr[0] = '!';
+// console.log(arr);
 
-// let a = 3;
-// alert(a);
+// let arr = ['a','b','c'];
+// arr[0] = 1;
+// arr[1] = -2;
+// arr[2] = -3;
+// console.log(arr);
+
+// let arr = ['a','b','c'];
+// arr[0] = arr[0] + '!';
+// arr[1] = arr[1] + '!';
+// arr[2] = arr[2] + '!';
+// console.log(arr);
+
+// let arr = ['a','b','c'];
+// arr[0] += '!';
+// arr[1] += '!';
+// arr[2] += '!';
+// console.log(arr);
+
+// let arr = [1,2,3,4,5,6];
+// arr[0] += 3;
+// arr[1] += 3;
+// arr[2] += 3;
+// arr[3] += 3;
+// arr[4] += 3;
+// arr[5] += 3;
+// console.log(arr);
 
-// let a;
-// a = 3;
-// alert(a);
+// let arr = [1,2,3,4];
+// arr[0]++;
+// ++arr[1];
+// arr[2]--;
+// --arr[3];
+// console.log(arr);
 
-// let num  = 123;
-// alert(num);
+// let arr = [1,2,3];
+// arr[0]++;
+// arr[1]++;
+// arr[2]++;
+// console.log(arr);
 
-// let a = 1;
-// let b = 2;
-// let c = 3;
+// let arr = [];
+// arr[0] = 'a';
+// arr[1] = 'b';
+// arr[2] = 'c';
+// console.log(arr);
 
-// let a = 1, b = 2, c = 3;
+// let arr = ['a','b','c'];
+// arr[3] = 'd';
+// console.log(arr);
 
-// let a, b, c;
-// a = 1;
-// b = 2;
-// c = 3;
+// let arr = [];
+// arr[0] = 1;
+// arr[1] = 2;
+// arr[2] = 3;
+// console.log(arr);
 
-// let a;
-// a = 1;
-// alert(a);
-// a = 2;
-// alert(a);
+// let arr = [1,2,3];
+// arr[3] = 4;
+// arr[4] = 5;
+// console.log(arr);
 
-// let a = 10;
-// alert(a);
-// a = 20;
-// alert(a);
+// let arr = ['a','b','c'];
+// console.log(arr[10]);
 
-// let a = 1 + 2;
-// alert(a);
+// let arr = [1,2,3];
+// arr[3] = '!';
+// console.log(arr);
 
-// let b = 3 - 2;
-// alert(b);
+// let arr = [1,2,3];
+// arr[4] = '!';
+// console.log(arr);
 
-// let c = 3 * 2;
-// alert(c);
 
-// let d = 4 / 2;
-// alert(d);
+// let arr = [1,2,3];
+// arr[5] = '!';
+// console.log(arr);
 
-// let a = 1 + 2 + 3;
-// alert(a);
+// let arr = [];
+// arr[3] = 'a';
+// arr[8] = 'b';
+// console.log(arr.length);
 
-// let a = 1;
-// let b = 2;
-// alert(a + b);
+// let obj = {1: 'a', 2: 'b', 3: 'c'};
+// console.log(obj);
 
-// let a = 1;
-// let b = 2;
-// let c = a + b;
-// alert(c);
+// let obj = {a:1, b:2, c:3};
+// console.log(obj['a']+obj['b']+obj['c']);
 
-// let a = 10;
-// let b = 2;
-// alert(a+b);
-// alert(a-b);
-// alert(a*b);
-// alert(a/b);
+// let obj = {'1a':1,'2b':2,'c-c':3};
+// console.log(obj['1a']+obj['2b']+obj['c-c']);
 
-// let c = 10;
-// let d = 5;
-// let result = c + d;
-// alert(result);
+// let obj = {'1a':1, b2:2,'c-c':3,d4:4};
 
-// let a = 1,
-// b = 2,
-// c = 3;
-// alert(a+b+c);
+// let obj = {key1:1, key2:2, key3:3};
+// console.log(obj.key1+obj.key2+obj.key3);
 
-// let a = 10, b = 5, c = a - b, d = 7, result = c + d;
-// alert(result);
+// let user = {name:'Alex',surname:'Ruzh',patronymic:'Alexs'};
+// console.log(user.name +' '+user.surname +' '+user.patronymic);
 
-// let a = 5 + 5 * 3;
-// alert(a);
+// let date = {year:'2022',month:'03',day:'24'};
+// console.log(date.year+'-'+date.month+'-'+date.day);
 
-// let a = 5 + 5 * 3 + 3;
-// alert(a);
+// let obj = {};
+// obj.a = 1;
+// obj.b = 2;
+// obj.c = 3;
+// console.log(obj);
 
-// let a = 8 / 2 + 2;
-// alert(a);
+// let arr = ['a', 'b','c'];
+// let key = 2;
+// console.log(arr[key]);
 
-// let a = 8 + 2 / 2;
-// alert(a);
+// let arr = [1,2,3,4,5];
+// let key1 = 1;
+// let key2 = 2;
+// console.log(arr[key1]+arr[key2]);
 
-// let a = 8 / 2 * 2;
-// alert(a);
+// let obj = {'a':1,'b':2,'c':3};
+// let key = 'b';
+// console.log(obj[key]);
 
-// let a = 8 * 4 / 2 / 2;
-// alert(a);
+// console.log( typeof {a: 1, b: 2, c: 3} );
 
-// let a = 2 * (2 + 4 * (3 + 1));
-// alert(a);
+// console.log( typeof [1, 2, 3] );
 
-// let a = (2 + 3) * (2 + 3);
-// alert(a);
+// let arr = [1, 2, 3];
+// console.log( typeof arr );
 
-// let a = (2 + 3) * 2 + 3;
-// alert(a);
+// let arr = [1, 2, 3];
+// console.log( typeof arr[0] );
 
-// let a = 2 * (2 + 4 * (3 + 1));
-// alert(a);
+// let arr = ['1', '2', '3'];
+// console.log( typeof arr[0] );
 
-// let a = 2 * 8 / 4;
-// alert(a);
+// console.log( Array.isArray([1, 2, 3]) );
 
-// let a = (2 * 8) / 4;
-// alert(a);
+// console.log( Array.isArray({a: 1, b: 2, c: 3}) );
 
-// let a = 2 * (8 / 4);
-// alert(a);
+// let arr1 = [1, 2, 3];
+// let arr2 = arr1;
+// arr1[0] = 'a';
+// console.log(arr2);
 
-// let a = 0.5 + 0.5;
-// alert(a);
+// let arr1 = [1, 2, 3];
+// let arr2 = arr1;
+// arr1[0] = 'a';
+// arr2[1] = 'b';
+// console.log(arr1);
 
-// let a = 1.5, b = 0.75, result = a + b;
-// alert( result );
+// let arr1 = [1, 2, 3];
+// let arr2 = arr1;
+// arr1[0] = 'a';
+// arr2[0] = 'b';
+// console.log(arr2);
 
-// let a = -100;
-// alert(a);
+// let obj = {a: 1, b: 2, c: 3, d: 4, e: 5};
+// console.log('e' in obj);
+// console.log('f' in obj);
 
-// let a = 100;
-// alert(-a);
+// let obj = {a: 1, b: 2, c: 3, d: 4, e: 5};
+// delete obj.e;
+// console.log('e' in obj);
 
-// alert(10 % 3);
+// let arr = ['a','b','c','d','e'];
+// delete arr[1];
+// delete arr[2];
+// console.log(arr);
+// console.log(arr.length);
 
-// alert(10% 2);
+// const arr = ['a', 'b', 'c'];
+// arr[1] = '!';
+// console.log(arr);
 
-// let a = 10, b =3;
-// alert( a % b);
+// const arr = ['a', 'b', 'c'];
+// arr = [1, 2, 3];
+// console.log(arr);
 
-// let a = 13; 
-// let b = 5;
-// alert( a % b);
+// const arr = ['a', 'b', 'c'];
+// arr = ['a', 'b', 'c'];
+// console.log(arr);
 
-// alert( 10 ** 3);
+// const arr = [1, 2, 3, 4, 5];
+// const res = arr[1] + arr[2];
+// console.log(res);
 
-// let a = 10;
-// alert( a ** 3);
+// let arr = [1, 2, 3, 4, 5];
+// console.log(arr[arr.length-1]);
 
-// let a = 10;
-// let b = 3;
-// alert(a ** b);
+// let arr = [1, 2, 3, 4, 5];
+// console.log(arr[0] + arr[1] + arr[2] + arr[3] + arr[4]);
 
-// let res = 2 ** 10;
-// alert(res);
+// let arr = [1, 2, 3, 4, 5];
+// console.log(arr.length);
 
-// let a = 3 * 2 ** 3;
-// alert(a);
+// let obj = {a: 1, b: 2, c: 3};
+// console.log(obj['a']);
 
-// let a = (3 * 2) ** 3;
-// alert(a);
+// let obj = {a: 1, b: 2, c: 3};
+// let key = 'a';
+// console.log(obj[key]);
 
-// let a = 3 * 2 ** (3 + 1);
-// alert(a);
-
-// let a = 2 ** 3 * 3;
-// alert(a);
-
-// let a = 3 * 2 ** 3 * 3;
-// alert(a);
-
-// let name = 'Alex', family = "Ruz";
-// alert(name + ' ' + family);
-
-// let str = 'abc' + 'def';
-// alert(str);
-
-// let str = '!!!';
-// alert(str);
-
-// let a = 'java', b = 'script';
-// alert(a + b);
-
-// let a = 'hello', b = 'world';
-// alert(a + ' ' + b);
-
-// let str = 'abcde';
-// alert(str.length);
-
-// let a = 'hello world';
-// alert(a.length);
-
-// let str1 = 'xxx';
-// let str2 = 'yyy';
-// let txt = `aaa ${str1} bbb ${str2} ccc`;
-// alert(txt);
-
-// let str = `a
-// b
-// c`;
-// alert(str);
-
-// let a;
-// alert(a);
-
-// let a = null;
-// alert(a);
-
-// let a = true;
-// alert(a);
-
-// let a = false;
-// alert(a);
-
-// alert('abc' * 3);
-
-// alert(NaN * 3);
-
-// let a = 'str1', b = 'str2';
-// alert(a*b);
-
-// let a = 10 / 0;
-// alert(a);
-
-// let a = -10 / 0;
-// alert(a);
-
-// console.log(123);
-
-// let num = 123;
-// console.log(num);
-
-// let num1 = 123;
-// let num2 = 456;
-// console.log(num1);
-// console.log(num2);
-
-// let num1 = 123;
-// let num2 = 456;
-// console.log(num1, num2);
-
-// let num = 10;
-// console.log(num);
-
-// let a = 10, b = 11, c = 12;
-// console.log(a, b, c);
-
-// console.log(123);
-// console.log('123');
-
-// alert(eee);
-
-// const pi = 3.14;
-
-// const PI = 3.14;
-// let r = 5, c =2*r*PI;
-// alert(c);
-
-// let a = '5' + '2';
-// alert(a);
-
-// let a = '5' + 2;
-// alert(a);
-
-// let a = 5 + '2';
-// alert(a);
-
-// let a = 5 + 2;
-// alert(a);
-
-// let a = '5' * '2';
-// alert(a);
-
-// let a = '5' - '2';
-// alert(a);
-
-// let a = '5' / '2';
-// alert(a);
-
-// let a = '5' % '2';
-// alert(a);
-
-// let a = '5s' * '2';
-// alert(a);
-
-// let a = '5s' + '2';
-// alert(a);
-
-// let a = (-'5') + (-'2');
-// alert(a);
-
-// let a = '5' * 1 + '2' * 1;
-// alert(a);
-
-// let a = '5' * '1' + '2' * '1';
-// alert(a);
-
-// let a = '' + 3 + 1;
-// alert(a);
-
-// let a ='2';
-// let b ='3';
-// alert(Number(a)+ Number(b));
-
-// let a = '10';
-// let b = '20';
-// alert(Number(a) + Number(b));
-
-// alert( Number('2') + Number('3') );
-
-// alert( 2 + Number('3') );
-
-// alert( '2' + Number('3') );
-
-// let a = +'2';
-// let b = +'3';
-// alert(a + b);
-
-// let num = parseInt('12px');
-// alert(num);
-
-// let num = parseFloat('12.5px');
-// alert(num);
-
-// let a = '5px', b = '6px';
-// let c = parseInt(a) + parseInt(b);
-// alert(c);
-
-// let a = '5.5px', b = '6.25px';
-// let c = parseFloat(a) + parseFloat(b);
-// alert(c);
-
-// let a = '5.5px', b = '6.25px';
-// let c = parseFloat(a) + parseFloat(b);
-// alert(c + 'px');
-
-// let a = 1, b = 2;
-// alert(String(a) + String(b));
-
-// let a = 1234543;
-// alert(String(a).length);
-
-// let a = 123, b = 23455;
-// alert(String(a).length + String(b).length);
-
-// alert(true + 3);
-
-// alert(true + true);
-
-// alert(true - true);
-
-// alert(true + false);
-
-// alert('1' + true);
-
-// alert( String(true) + 1 );
-
-// alert( String(true) + Number(true) );
-
-// let test = Boolean(3);
-// alert(test);
-
-// let test = Boolean(0);
-// alert(test);
-
-// let test = Boolean(-1);
-// alert(test);
-
-// let test = Boolean(-0);
-// alert(test);
-
-// let test = Boolean(+0);
-// alert(test);
-
-// let test = Boolean('abc');
-// alert(test);
-
-// let test = Boolean('');
-// alert(test);
-
-// let test = Boolean('0');
-// alert(test);
-
-// let test = Boolean(true);
-// alert(test);
-
-// let test = Boolean(false);
-// alert(test);
-
-// let test = Boolean('true');
-// alert(test);
-
-// let test = Boolean('false');
-// alert(test);
-
-// let test = Boolean(null);
-// alert(test);
-
-// let test = Boolean('null');
-// alert(test);
-
-// let test = Boolean(undefined);
-// alert(test);
-
-// let test = Boolean('undefined');
-// alert(test);
-
-// let test = Boolean(NaN);
-// alert(test);
-
-// let test = Boolean('NaN');
-// alert(test);
-
-// let test = Boolean(3 * 'abc');
-// alert(test);
-
-// let test = Boolean(Infinity);
-// alert(test);
-
-// let test = Boolean(1 / 0);
-// alert(test);
-
-// let str = 'abcde';
-// let a = str[0], b = str[2], c = str[4];
-// alert(a + b + c);
-
-// let str = 'abcde';
-// let str2 = str[4]+str[3]+str[2]+str[1]+str[0];
-// alert(str2);
-
-// let str = 'abcde';
-// let num = 3;
-// alert(str[num]);
-
-// let str = '12345';
-// alert(str[str.length-1]);
-
-// let str = '12345';
-// alert(str[str.length-2]);
-
-
-// let str = '12345';
-// alert(str[str.length-3]);
-
-// let str = '12345';
-// alert(Number(str[0]) + Number(str[1]) + Number(str[2]) + Number(str[3])+ Number(str[4]));
-
-// let str = String(12345);
-// alert(Number(str[0]) + Number(str[1]) + Number(str[2]) + Number(str[3])+ Number(str[4]));
-
-// let str = String(12345);
-// alert( str[0] * str[1] * str[2] * str[3] * str[4]);
-
-// let str = String(12345);
-// alert(str[4] + str[3] + str[2] + str[1] + str[0]);
-
-// let num = 1;
-// num = num + 1;
-// num = num + 1;
-// alert(num);
-
-// let num = 1;
-// num = num + 2;
-// num = num + 3;
-// alert(num);
-
-// let num = 47;
-// num += 7;
-// num -= 18;
-// num *= 10;
-// num /= 15;
-// alert(num);
-
-// let num = 3;
-// alert(++num);
-
-// let num = 3;
-// alert(num++);
-
-// let num = 3;
-// alert(--num);
-
-// let num = 3;
-// alert(num--);
-
-// let num1 = 3;
-// let num2 = ++num1;
-// alert(num1);
-// alert(num2);
-
-// let num1 = 3;
-// let num2 = num1++;
-// alert(num1);
-// alert(num2);
-
-// let num1 = 3;
-// let num2 = --num1;
-// alert(num1);
-// alert(num2);
-
-// let num1 = 3;
-// let num2 = num1--;
-// alert(num1);
-// alert(num2);
-
-// let num1 = 3;
-// num1++;
-// let num2 = num1--;
-// alert(num1++);
-// alert(--num2);
-
-// alert(0.1 * 0.2);
-// alert(0.3 - 0.1);
-
-// let a = 0.3 - 0.1;
-// alert(a.toFixed(2));
-
-// let name = prompt('Ваше имя?');
-// alert('Ваше имя: ' + name);
-
-// alert('Ваш возраст: ' + prompt('Сколько вам лет?'));
-
-// let num1 = +prompt('Введите 1 число');
-// let num2 = +prompt('Введите 2 число');
-// alert(num1 + num2);
-
-// let a = +prompt('Введите сторону квадрата'),
-// result = a*a;
-// alert(result);
-
-// let a = +prompt('Введите длину пряумоугольника'),
-// b = +prompt('Введите ширину прямоугольника'),
-// res = a * b;
-// alert(res);
-
-// document.write(123);
-
-// document.write('text');
-
-// document.write('<b>text</b>');
-
-// let str = 'text';
-// document.write(str);
-
-// let str = 'text';
-// document.write('<b>' + str + '</b>');
-
-// document.write('text <br> text');
-
-// document.write('text1<br>');
-// document.write('text2<br>');
-// document.write('text3<br>');
-
-// document.write('<i>'+ 'lola' + '</i>');
-
-// let str = 'text';
-// document.write('<i>'+ str + '</i>');
-
-// let str = '1  <br>  2 <br>  3 <br>  4 <br>  5';
-// document.write(str);
-
-// let num1 = 1;
-// let num2 = 2;
-// console.log('сумма: ' + (num1 + num2));
-
-// let a = 1;
-// let b = 2;
-// let c = 3;
-// console.log(a + b + c);
-
-// let num = '123';
-// let sum = Number(num[0]) + Number(num[1]) + +num[2];
+// let obj = {a: 1, b: 2, c: 3};
+// let sum = obj['a'] + obj['b'] + obj['c'];
 // console.log(sum);
 
-// let num = String(123);
-// console.log(num[0]);
-
-// let a = 0;
-// console.log(++a);
-
-// let num = String(123);
-// console.log(num.length);
-
-// let a = 24 * 60 * 60;
-// console.log(a);
-
-// let num = 123;
-// let str = String(num);
-// console.log(str.length);
-
-// let num = 123;
-// let str = String(num);
-// console.log(str[str.length - 1]);
-
-// let num = 123;
-// let str = String(num);
-// console.log(str.length);
-
-// let num = 123;
-// let str = String(num);
-// console.log(str[str.length - 1]);
-
-// let a = '123';
-// let b = '456';
-// let s = Number(a) + Number(b);
-// console.log(s);
-
-// let aaa = 1;
-// let bbb = 2;
-// let ссс = 3;
-// console.log(aaa + bbb + ссс);
-
-// alert(60* 60);
-// alert(60 * 60 * 24);
-// alert(60 * 60 * 24 * 30);
-// alert(60 * 60 * 24 * 30 * 12);
-
-// alert(60 * 24);
-// alert(60 * 24 * 30 * 12);
-// alert(1024*1024);
-// alert(1024*1024*1024);
-// alert(1024*1024*1024 * 10);
-// alert(1024*1024*1024*1024);
-// alert(1024*1024*1024);
-
-// let r = 10, PI = 3.14, s = r**2*PI;
-// alert(s);
-
-// let a = 10, s = a*a;
-// alert(s);
-
-// let a = 10, b = 12, s = a * b;
-// alert(s);
-
-// let a = 10, b = 12, p = a + a + b + b;
-// alert(p);
-
-// let tc =14, tf = (tc * 9 / 5) + 32;
-// alert(tf);
-
-let tf =14, tc = (tf - 32) *5 / 9;
-alert(tc);
-
-
+// let obj = {a: 1, b: 2, c: 3,d:4,f:6,h:9};
+// console.log(Object.keys(obj).length);
